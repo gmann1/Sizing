2a459d55c41d4f70edc533bf9349e2088973b9fd
zcmoore <zcmoore@asu.edu>
2013-12-26 23:05:34 -0700

    Added ordinal numbering

--- src/main/java/edu/asu/voctec/GameDefaults.java
+++ src/main/java/edu/asu/voctec/GameDefaults.java
@@ -59,13 +59,13 @@ public interface GameDefaults
 			public static final String STEP_THREE = "resources/default/img/taskScreen/Task3.png";
 			public static final String STEP_FOUR = "resources/default/img/taskScreen/Task4.png";
 			public static final String STEP_FIVE = "resources/default/img/taskScreen/Task5.png";
-
+			
 			public static final String STEP_ONE_COMPLETE = "resources/default/img/taskScreen/Task1Complete.png";
 			public static final String STEP_TWO_COMPLETE = "resources/default/img/taskScreen/Task2Complete.png";
 			public static final String STEP_THREE_COMPLETE = "resources/default/img/taskScreen/Task3Complete.png";
 			public static final String STEP_FOUR_COMPLETE = "resources/default/img/taskScreen/Task4Complete.png";
 			public static final String STEP_FIVE_COMPLETE = "resources/default/img/taskScreen/Task5Complete.png";
-
+			
 			public static final String STEP_ONE_SELECTED = "resources/default/img/taskScreen/Task1Current.png";
 			public static final String STEP_TWO_SELECTED = "resources/default/img/taskScreen/Task2Current.png";
 			public static final String STEP_THREE_SELECTED = "resources/default/img/taskScreen/Task3Current.png";
@@ -81,16 +81,21 @@ public interface GameDefaults
 			public static final String PV_SIZING = "resources/default/img/selector/icons/pvSizing.png";
 			public static final String CONTROLLER_SIZING = "resources/default/img/selector/icons/controllerSizing.png";
 		}
-
+		
 		public interface SelectorDisplayBorders
 		{
 			public static final String DEFAULT = "resources/default/img/selector/display/EmptyBox.png";
 			public static final String HIGHLIGHTED = "resources/default/img/selector/display/EmptyBoxHighlighted.png";
 			public static final String CORRECT = "resources/default/img/selector/display/EmptyBoxCorrect.png";
 			public static final String INCORRECT = "resources/default/img/selector/display/EmptyBoxWrong.png";
-
+			
 			public static final String SMALL_ARROW = "resources/default/img/selector/display/smallArrow.png";
 			public static final String LARGE_ARROW = "resources/default/img/selector/display/largeArrow.png";
+		}
+		
+		public interface Selector
+		{
+			public static final String HINT_BOX_BACKGROUND = "resources/default/img/selector/hintBox.png";
 		}
 		
 	}
@@ -99,6 +104,39 @@ public interface GameDefaults
 	{
 		// Critical Design Month Resources
 		public static final String CHARGE = "resources/default/sounds/charge.wav";
+	}
+	
+	public interface Fonts
+	{
+		public static final String FONT_NAME = "Meiryo"; // "Hiragino Kaku Gothic Pro";
+		public static final int FONT_SIZE = 36;
+		public static final boolean ANTI_ALLIAS = true;
+		public static final Color FONT_COLOR = Color.magenta;
+		
+		public static final Font AWT_FONT = new Font(FONT_NAME, Font.BOLD,
+				Fonts.FONT_SIZE);
+		public static final TrueTypeFont TRUE_TYPE_FONT = new TrueTypeFont(
+				AWT_FONT, ANTI_ALLIAS);
+	}
+	
+	public interface SelectorDefaults
+	{
+		public static Dimension ARROW_SIZE = new Dimension(41, 123);
+		public static Dimension PRIMARY_SELECTION_SIZE = new Dimension(183, 184);
+		public static Dimension SECONDARY_SELECTION_SIZE = new Dimension(124,
+				123);
+		public static Dimension SHADOW_SIZE = new Dimension(494, 122);
+		
+		public static Point ARROW_LOCATION_LEFT = new Point(0, 91);
+		public static Point ARROW_LOCATION_RIGHT = new Point(535, 91);
+		public static Point PRIMARY_SELECTION_LOCATION = new Point(196, 0);
+		public static Point SECONDARY_SELECTION_LOCATION_LEFT = new Point(52,
+				91);
+		public static Point SECONDARY_SELECTION_LOCATION_RIGHT = new Point(399,
+				91);
+		public static Point SHADOW_LOCATION = new Point(41, 184);
+		public static Rectangle ICON_LABEL_BOUNDS = new Rectangle(0, 200, 183,
+				75);
 	}
 	
 	public interface XMLPaths
@@ -147,34 +185,22 @@ public interface GameDefaults
 					"Rotate Panel: 'q' and 'r' keys");
 		}
 		
-	}
-	
-	public interface Fonts
-	{
-		public static final String FONT_NAME = "Meiryo"; // "Hiragino Kaku Gothic Pro";
-		public static final int FONT_SIZE = 36;
-		public static final boolean ANTI_ALLIAS = true;
-		public static final Color FONT_COLOR = Color.magenta;
+		// Numbers
+		public interface OrdinalNumbers
+		{
+			public static final LabelName FIRST = new LabelName("ordinal1",
+					"first");
+			public static final LabelName SECOND = new LabelName("ordinal2",
+					"second");
+			public static final LabelName THIRD = new LabelName("ordinal3",
+					"thrid");
+			public static final LabelName FOURTH = new LabelName("ordinal4",
+					"fourth");
+			public static final LabelName FIFTH = new LabelName("ordinal5",
+					"fifth");
+			public static final LabelName SIXTH = new LabelName("ordinal6",
+					"sixth");
+		}
 		
-		public static final Font AWT_FONT = new Font(FONT_NAME,
-				Font.BOLD, Fonts.FONT_SIZE);
-		public static final TrueTypeFont TRUE_TYPE_FONT = new 
-				TrueTypeFont(AWT_FONT, ANTI_ALLIAS);
-	}
-	
-	public interface SelectorDefaults
-	{
-		public static Dimension ARROW_SIZE = new Dimension(41, 123);
-		public static Dimension PRIMARY_SELECTION_SIZE = new Dimension(183, 184);
-		public static Dimension SECONDARY_SELECTION_SIZE = new Dimension(124, 123);
-		public static Dimension SHADOW_SIZE = new Dimension(494, 122);
-		
-		public static Point ARROW_LOCATION_LEFT = new Point(0, 91);
-		public static Point ARROW_LOCATION_RIGHT = new Point(535, 91);
-		public static Point PRIMARY_SELECTION_LOCATION = new Point(196, 0);
-		public static Point SECONDARY_SELECTION_LOCATION_LEFT = new Point(52, 91);
-		public static Point SECONDARY_SELECTION_LOCATION_RIGHT = new Point(399, 91);
-		public static Point SHADOW_LOCATION = new Point(41, 184);
-		public static Rectangle ICON_LABEL_BOUNDS = new Rectangle(0, 200, 183, 75);
 	}
 }
