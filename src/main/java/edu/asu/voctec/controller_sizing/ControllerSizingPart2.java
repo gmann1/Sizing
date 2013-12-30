6d73e4fc4966b094c343653adb30f1f3cc7276ff
zcmoore <zcmoore@asu.edu>
2013-12-30 15:17:29 -0700

    Updated transitions - Task 5 now links properly

--- src/main/java/edu/asu/voctec/controller_sizing/ControllerSizingPart2.java
+++ src/main/java/edu/asu/voctec/controller_sizing/ControllerSizingPart2.java
@@ -1,5 +1,20 @@
 package edu.asu.voctec.controller_sizing;
 
-public class ControllerSizingPart2 {
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+import edu.asu.voctec.game_states.GUI;
+
+public class ControllerSizingPart2 extends GUI
+{
+
+	@Override
+	public void init(GameContainer container, StateBasedGame game)
+			throws SlickException
+	{
+		// TODO Auto-generated method stub
+		
+	}
 
 }
