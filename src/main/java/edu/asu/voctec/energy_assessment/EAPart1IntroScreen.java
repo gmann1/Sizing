8222e0c51ca9c3c739628dbe86f1ac3a56a62f05
cacarpe1 <cacarpe1@asu.edu>
2014-01-06 03:33:22 -0700

    Commit

--- src/main/java/edu/asu/voctec/energy_assessment/EAPart1IntroScreen.java
+++ src/main/java/edu/asu/voctec/energy_assessment/EAPart1IntroScreen.java
@@ -1,5 +1,69 @@
 package edu.asu.voctec.energy_assessment;
 
+import java.awt.Rectangle;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+import edu.asu.voctec.GUI.*;
+import edu.asu.voctec.game_states.GUI;
+import edu.asu.voctec.game_states.TaskScreen;
+import edu.asu.voctec.utilities.Position;
+
+public class EAPart1IntroScreen extends GUI
+{
+	public static final String ARROW_RIGHT = "resources/default/img/arrow-right.png";
+	public static final String INTRODUCTION = "In this game you need will need to put in order the different processes needed to determine the correct daily energy requirements.";
+	
+	@Override
+	public void init(GameContainer container, StateBasedGame game)
+			throws SlickException
+	{
+		this.backgroundImage = new Image(ImagePaths.MainMenuBackground);
+		Rectangle textLocation = new Rectangle(0, 50, 300, 50);
+		
+		// Title
+		TextField welcomeLabel = new TextField(textLocation, 0.95f, "Welcome!",
+				TextDisplay.FormattingOption.FIT_TEXT);
+		welcomeLabel.setFontColor(Fonts.FONT_COLOR);
+		welcomeLabel.center();
+		
+		// Introduction Body
+		textLocation = new Rectangle(150, 200, 500, 400);
+		TextArea introductionText = new TextAreaX(textLocation, 0.95f,
+				INTRODUCTION);
+		introductionText.setFontSize(12f);
+		introductionText.setFontSize(Fonts.FONT_SIZE_LARGE);
+		introductionText.setFontColor(Fonts.FONT_COLOR);
+		
+		// Start Button
+		Button startButton = new Button(new Image(ARROW_RIGHT), 750, 550,
+				new Rectangle(0, 0, 50, 25), "Begin!");
+		startButton.setFontColor(Fonts.TRANSITION_FONT_COLOR);
+		startButton.addActionListener(new TransitionButtonListener(EAPart1.class));
+		startButton.positionText(Position.LEFT);
+		
+		// Back Button
+		Button backButton = new Button(new Image(ImagePaths.BACK_BUTTON), 5, 5,
+				new Rectangle(0, 0, 50, 25), "Back");
+		backButton.addActionListener(new TransitionButtonListener(TaskScreen.class));
+		backButton.setFontColor(Fonts.TRANSITION_FONT_COLOR);
+		backButton.positionText(Position.RIGHT);
+		
+		// Add all components to this menu
+		this.addComponent(startButton);
+		this.addComponent(welcomeLabel);
+		this.addComponent(introductionText);
+		this.addComponent(backButton);
+
+	}
+	
+}
+
+
+/*
 import java.awt.Dimension;
 import java.awt.Rectangle;
 import java.util.Arrays;
@@ -75,4 +139,4 @@ public class EAPart1IntroScreen extends GUI
 			return null;
 		}
 		
-	}
+	}*/
