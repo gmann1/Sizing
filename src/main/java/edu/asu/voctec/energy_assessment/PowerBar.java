f6d056fd7ea356e6c8e6e6d74752ed65eb8e2c28
malyahy2 <malyahy2@asu.edu>
2014-01-10 10:24:16 -0700

    pictures and a little change

--- src/main/java/edu/asu/voctec/energy_assessment/PowerBar.java
+++ src/main/java/edu/asu/voctec/energy_assessment/PowerBar.java
@@ -4,7 +4,6 @@ import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
 import edu.asu.voctec.GUI.BasicComponent;
-import edu.asu.voctec.game_states.GUI;
 
 public class PowerBar extends BasicComponent{
 	
@@ -12,13 +11,13 @@ public class PowerBar extends BasicComponent{
 	private static final String POWER_BAR_BACKGROUND_CORRECT = "resources/default/img/minigames/energyAssessment/PowerBar/PowerBarBKGDrCorrect.png";
 	private static final String POWER_BAR_INDICATOR = "resources/default/img/minigames/energyAssessment/PowerBar/PowerBarIndicatorCorrect.png";
 	
-	public BasicComponent powerBar;
-	private Image powerBarBackgroundCorrectImage;
+	public BasicComponent powerBarIndicator;
+	private Image powerBarBackgroundImage, powerBarBackgroundCorrectImage;
 	private int powerBarRelativeX = 16, powerBarRelativeY = 382, powerBarMaxHeight = 366, powerBarWidth = 75, powerBarHeight = 0;
 	private static int powerBarBackgroundWidth = 107, powerBarBackgroundHeight = 426;
 	private int minLimit = 0, maxLimit = 0 , maxPower = 0;
 	
-	public PowerBar(int x, int y, double scale, int minWinningLimit, int maxWinningLimit, GUI gameClass)
+	public PowerBar(int x, int y, double scale, int minWinningLimit, int maxWinningLimit)
 			throws SlickException {
 		
 		super(POWER_BAR_BACKGROUND, x, y, (int) (powerBarBackgroundWidth*scale), (int) (powerBarBackgroundHeight*scale));
@@ -26,22 +25,25 @@ public class PowerBar extends BasicComponent{
 		minLimit = minWinningLimit;
 		maxLimit = maxWinningLimit;
 		maxPower = (int) (minLimit+(0.28*minLimit));
-		//powerBarWidth = (int) (powerBarWidth*scale);
+		powerBarWidth = (int) (powerBarWidth*scale);
 		
 		powerBarBackgroundCorrectImage = new Image(POWER_BAR_BACKGROUND_CORRECT);
+		powerBarBackgroundImage = new Image(POWER_BAR_BACKGROUND);
 		
-		powerBar = new BasicComponent(POWER_BAR_INDICATOR, x+powerBarRelativeX, y+powerBarRelativeY-powerBarHeight);
-		powerBar.resize(powerBarWidth,powerBarHeight);
-		//gameClass.addComponent(powerBar);
+		powerBarIndicator = new BasicComponent(POWER_BAR_INDICATOR, x+powerBarRelativeX, y+powerBarRelativeY-powerBarHeight);
+		powerBarIndicator.resize(powerBarWidth,powerBarHeight);
+		//gameClass.addComponent(powerBarIndicator);
 	}
 	
 	public void updatePowerBar(int totalPower)
 	{
 		powerBarHeight = translateIntoHeight(totalPower);
-		powerBar.resize(powerBarWidth,powerBarHeight);
-		powerBar.setY(getY()+powerBarRelativeY-powerBarHeight);
+		powerBarIndicator.resize(powerBarWidth,powerBarHeight);
+		powerBarIndicator.setY(getY()+powerBarRelativeY-powerBarHeight);
 		if(targetAchieved(totalPower))
 			this.setCurrentImage(powerBarBackgroundCorrectImage, true);
+		else
+			this.setCurrentImage(powerBarBackgroundImage, true);
 	}
 	
 	private int translateIntoHeight(int totalPower)
