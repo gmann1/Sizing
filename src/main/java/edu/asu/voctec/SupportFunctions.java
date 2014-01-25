9e26b6c3fa23d24fda1536d4c9b79b5757ae493d
zcmoore <zcmoore@asu.edu>
2014-01-24 17:02:29 -0700

    Disabled non-implemented buttons; still need to disable dev-access

---
+++ src/main/java/edu/asu/voctec/SupportFunctions.java
@@ -0,0 +1,33 @@
+package edu.asu.voctec;
+
+import edu.asu.voctec.GameDefaults.Fonts;
+import edu.asu.voctec.GameDefaults.IntroductionScreenDefaults;
+import edu.asu.voctec.GUI.TextAreaX;
+import edu.asu.voctec.GUI.TextDisplay;
+import edu.asu.voctec.GUI.TextField;
+
+public class SupportFunctions
+{
+	public static TextField generateWelcomeLabel(String text)
+	{
+		TextField welcomeLabel = new TextField(
+				IntroductionScreenDefaults.TITLE_BOUNDS, 0.95f, text,
+				TextDisplay.FormattingOption.CLIP_TEXT);
+		welcomeLabel.setFontSize(Fonts.FONT_SIZE_EXTRA_LARGE);
+		welcomeLabel.setFontColor(Fonts.FONT_COLOR);
+		welcomeLabel.center();
+		
+		return welcomeLabel;
+	}
+	
+	public static TextAreaX generateIntroductionDisplay(String text)
+	{
+		TextAreaX introductionText = new TextAreaX(
+				IntroductionScreenDefaults.BODY_TEXT_BOUNDS, 0.95f, text);
+		introductionText.setFontSize(Fonts.FONT_SIZE_MEDIUM);
+		introductionText.setFontSize(Fonts.FONT_SIZE_LARGE);
+		introductionText.setFontColor(Fonts.FONT_COLOR);
+		
+		return introductionText;
+	}
+}
