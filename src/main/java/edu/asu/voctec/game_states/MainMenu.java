299c823832a8c5ca58f7cddf7a9ac6b8f7ac2b2c
Zach Moore <zcmoore@asu.edu>
2014-05-21 12:58:09 -0700

    Simplified; implemented deferred loading

--- src/main/java/edu/asu/voctec/game_states/MainMenu.java
+++ src/main/java/edu/asu/voctec/game_states/MainMenu.java
@@ -2,7 +2,6 @@ package edu.asu.voctec.game_states;
 
 import java.awt.Rectangle;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
@@ -14,14 +13,10 @@ import org.newdawn.slick.state.StateBasedGame;
 
 import edu.asu.voctec.Game;
 import edu.asu.voctec.GameDefaults;
-import edu.asu.voctec.GUI.BasicComponent;
 import edu.asu.voctec.GUI.Button;
 import edu.asu.voctec.GUI.ButtonListener;
-import edu.asu.voctec.GUI.TextAreaX;
 import edu.asu.voctec.GUI.TextDisplay;
 import edu.asu.voctec.GUI.TextField;
-import edu.asu.voctec.information.ScenarioData;
-import edu.asu.voctec.information.TaskData;
 import edu.asu.voctec.information.UserProfile;
 import edu.asu.voctec.minigames.step_selection.ScenarioIntroductionScreen;
 import edu.asu.voctec.utilities.UtilFunctions;
@@ -29,25 +24,20 @@ import edu.asu.voctec.utilities.UtilFunctions;
 public class MainMenu extends GUI implements GameDefaults, KeyListener
 
 {
-	boolean nextState = false;
-	private int lc = 0;
-	private Button startButton;
-	private String userName = "";
-	Scanner reader = new Scanner(System.in);
+	private Button userNameField;
 	private Button readyButton;
-	private BasicComponent sidePanel;
-	private TextAreaX hintBox;
+	private String userName = "";
 	
 	public static ArrayList<String> UserData = new ArrayList<>();
 	
-	public class StartButtonListener extends ButtonListener
+	public class UserNameFieldListener extends ButtonListener
 	{
 		private static final long serialVersionUID = -325855184018136406L;
 		
 		@Override
 		protected void actionPerformed()
 		{
-			startButton.getTextField().setText(userName);
+			userNameField.getTextField().setText(userName);
 		}
 	}
 	
@@ -87,7 +77,7 @@ public class MainMenu extends GUI implements GameDefaults, KeyListener
 		else if (isBackspace(c) && (userName.length() > 0))
 			userName = userName.substring(0, userName.length() - 1);
 		
-		startButton.getTextField().setText(userName);
+		userNameField.getTextField().setText(userName);
 	}
 	
 	/**
@@ -127,62 +117,42 @@ public class MainMenu extends GUI implements GameDefaults, KeyListener
 	}
 	
 	@Override
-	public void keyReleased(int key, char c)
-	{
-		// Do nothing
-	}
-	
-	@Override
 	public void init(GameContainer container, StateBasedGame game) throws SlickException
 	{
+		System.out.println("\nMainMenu: Initializing...");
 		
-		Rectangle hintBounds = new Rectangle(600, 208, 192, 192);
-		Rectangle relativeHintTextBounds = UtilFunctions.dialateRectangle(new Rectangle(
-				0, 0, 192, 192), 0.92f);
-		hintBox = new TextAreaX(hintBounds, relativeHintTextBounds, null);
-		// System.out.println("\nMainMenu: Initializing...");
-		sidePanel = new BasicComponent(ImagePaths.SIDE_PANEL, 592, 0);
-		sidePanel.rescale(208, 600);
-		sidePanel.setX(592);
-		sidePanel.setY(0);
 		Image readyButtonImage = new Image(ImagePaths.READY_BUTTON);
 		Rectangle textBounds = UtilFunctions.getImageBounds(readyButtonImage);
 		textBounds = UtilFunctions.dialateRectangle(textBounds, 0.80f);
-		readyButton = new Button(readyButtonImage, sidePanel.getX()
-				+ sidePanel.getBounds().width / 2
-				- UtilFunctions.getImageBounds(readyButtonImage).width / 2,
-				hintBox.getY() + hintBox.getBounds().height + 50, textBounds, "Ready");
+		readyButton = new Button(readyButtonImage, 0, 0, textBounds, "Ready");
 		readyButton.setFontColor(Fonts.BUTTON_FONT_COLOR);
 		readyButton.addActionListener(new ReadyButtonListener());
 		
-		// TODO Replace with profile
+		// TODO Add support for loading profiles
 		UserProfile profile = new UserProfile("Default User");
-		ScenarioData scenario = profile.getScenario(0);
-		TaskData task = scenario.getEntryStep();
-		Game.setCurrentUser(profile);
-		Game.setCurrentScenario(scenario);
-		Game.setCurrentTask(task);
+		profile.activate();
 		
 		int buttonSpacing = 15;
 		int buttonWidth = 350;
 		int buttonHeight = 75;
+		// Account for square icon at the far right
+		int textWidth = buttonWidth - buttonHeight;
 		float borderScale = 0.9f;
 		
 		// Determine text and button bounds, relative to each button
 		Rectangle buttonBounds = new Rectangle(0, 0, buttonWidth, buttonHeight);
-		Rectangle relativeTextBounds = new Rectangle(0, 0, buttonWidth - buttonHeight,
-				buttonHeight);
+		Rectangle relativeTextBounds = new Rectangle(0, 0, textWidth, buttonHeight);
 		relativeTextBounds = UtilFunctions.dialateRectangle(relativeTextBounds,
 				borderScale);
 		
-		// Declare Buttons
-		// Start Button
-		startButton = new Button(ImagePaths.NEW_GAME_BUTTON, buttonBounds,
+		// Declare Components
+		// UserName Field
+		userNameField = new Button(ImagePaths.NEW_GAME_BUTTON, buttonBounds,
 				relativeTextBounds, "Click to enter username");
-		startButton.addActionListener(new StartButtonListener());
+		userNameField.addActionListener(new UserNameFieldListener());
 		
 		// Color text
-		startButton.setFontColor(MainMenuDefaults.BUTTON_FONT_COLOR);
+		userNameField.setFontColor(MainMenuDefaults.BUTTON_FONT_COLOR);
 		
 		// Game Title
 		Rectangle textLocation = new Rectangle(0, 0, 2 * buttonWidth, buttonHeight);
@@ -197,17 +167,17 @@ public class MainMenu extends GUI implements GameDefaults, KeyListener
 		// Add buttons to this menu
 		this.addComponent(gameTitle);
 		this.addComponent(padding);
-		this.addComponent(startButton);
+		this.addComponent(userNameField);
 		this.addComponent(readyButton);
 		
 		this.centerComponentsStacked(buttonSpacing);
 		gameTitle.setY(gameTitle.getY() - 100);
-		startButton.setY(startButton.getY() - 50);
+		userNameField.setY(userNameField.getY() - 50);
 		readyButton.setY(readyButton.getY() - 35);
 		Image background = new Image(ImagePaths.MainMenuBackground);
 		setBackgroundImage(background.getScaledCopy(800, 600));
 		
-		// System.out.println("MainMenu: Initialization Finished.\n");
+		System.out.println("MainMenu: Initialization Complete.\n");
 	}
 	
 	@Override
@@ -218,7 +188,8 @@ public class MainMenu extends GUI implements GameDefaults, KeyListener
 		try
 		{
 			GameState newState = new ScenarioIntroductionScreen();
-			Game.getCurrentGame().addState(newState, Game.getCurrentGame().getContainer());
+			Game.getCurrentGame()
+					.addState(newState, Game.getCurrentGame().getContainer());
 		}
 		catch (SlickException e)
 		{
